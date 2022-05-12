0a0096246a10e551b49fe2e765e7d9e08240ed2b
timbaron <timothy.akiode@student.aul.edu.ng>
2022-05-12 22:43:32 +0100

    getting started with nextJs.

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                This is a simple boilerplate for creating React applications.
+            </p>
+        </div>
+    );
+}
+
+export default About;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,6 @@
+const Text = () => {
+    return (<div>
+        <h1>Test</h1>
+    </div>);
+    }
+    export default Text;
